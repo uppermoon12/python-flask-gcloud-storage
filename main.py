2b4cec6e2c19b930ce95544b4f3f5620c3457978
ZiyadZK <kakangtea74@gmail.com>
2023-12-16 23:15:51 +0700

    first push

--- main.py
+++ main.py
@@ -14,5 +14,17 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+@app.route('/connect', methods=['GET'])
+def connect():
+    service_account = json.loads(os.environ.get("GOOGLE_CLOUD_CREDENTIALS"))
+    creds = Credentials.from_service_account_info(service_account)
+    client = storage.Client(credentials=creds)
+    bucket = client.bucket('skillshift-bucket')
+
+    base_images_dir = 'photos/freelancers/freelancer_blabla123ganteng/base_images/'
+    base_images = bucket.list_blobs(prefix=base_images_dir)
+    return jsonify([base_image.name for base_image in base_images])
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
